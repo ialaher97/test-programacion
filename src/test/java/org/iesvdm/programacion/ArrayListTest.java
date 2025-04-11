cde60230bf5121b286b740b9f79fe224a589672f
ialaher97 <ivan.alarcon.herrera@gmail.com>
2025-04-11 09:25:18 +0200

    11/04

--- src/test/java/org/iesvdm/programacion/ArrayListTest.java
+++ src/test/java/org/iesvdm/programacion/ArrayListTest.java
@@ -18,15 +18,69 @@ public class ArrayListTest {
 		arrayList.add(str1);
 		arrayList.add(str2);
 		
+		//Elimino str2 que tiene index 1
+		arrayList.remove(1);
+		
 		//Expect:
-		Assert.assertEquals("[cad1,cad2]", arrayList.toString());
-		Assert.assertEquals(2, arrayList.size());
+		Assert.assertEquals("[Cadena1]", arrayList.toString());
+		Assert.assertEquals(1, arrayList.size());
 		
 		int posStr1 = arrayList.indexOf(str1);
 		Assert.assertEquals(0, posStr1);
 		
-		String strPos1 = arrayList.get(1);
-		Assert.assertEquals(str2,strPos1);
+		String strPos1 = arrayList.get(0);
+		Assert.assertEquals(str1,strPos1);
+		
+		try {
+			String strPos = arrayList.get(1);
+			
+			
+		}catch (IndexOutOfBoundsException e) {
+			Assert.assertEquals("Index 1 out of bounds for length 1", e.getMessage());
+		}
+		
+		
+		
+		
+	}
+	@Test
+	public void removeByObejctInArrayListTest1() {
+		
+		//Given:
+		ArrayList<String> arrayList = new ArrayList<>();
+		String str1 = "Cadena1";
+		String str2 = "Cadena2";
+		
+		//When:
+		arrayList.add(str1);
+		arrayList.add(str2);
+		
+		Assert.assertEquals(2, arrayList.size());
+
+		//Elimino str2
+		arrayList.remove(str2);
+		
+		//Expect:
+		Assert.assertEquals("[Cadena1]", arrayList.toString());
+		Assert.assertEquals(1, arrayList.size());
+		
+		int posStr1 = arrayList.indexOf(str1);
+		Assert.assertEquals(0, posStr1);
+		
+		String strPos1 = arrayList.get(0);
+		Assert.assertEquals(str1,strPos1);
+		
+		try {
+			String strPos = arrayList.get(1);
+			
+			
+		}catch (IndexOutOfBoundsException e) {
+			Assert.assertEquals("Cadena1",strPos1);
+		}
+		
+		
+		
+		
 	}
 
 }
